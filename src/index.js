674c223271f42ea015090bcf95fd46f86fbcd05e
Kien <ntkien123@gmail.com>
2023-07-30 01:49:07 -0700

    update code

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars'); // Use exphbs instead of handlebars
+const app = express();
+const port = 3000;
+// static file
+app.use(express.static(path.join(__dirname,'public')))
+app.use(morgan('combined'));
+console.log(__dirname)
+// Template engine setup
+app.engine('hbs', exphbs.engine({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
